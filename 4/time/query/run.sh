fdda38e687f6730345ab1805543334a649aebe7b
AlvaroGuzman <aguzman2017@udec.cl>
2025-11-09 20:40:48 -0300

    Boletin 3 y 4

--- 4/time/query/run.sh
+++ 4/time/query/run.sh
@@ -2,8 +2,8 @@
 
 
 g++ uhr_segment.cpp -std=c++11 -O0 -o uhr_segment;
-./uhr_segment time_query_segment_tree.csv 32 1 33554432 2;
+./uhr_segment time_query_segment_tree.csv 32 1 1073741824 2;
 
 
 g++ uhr_sparse.cpp -std=c++11 -O0 -o uhr_sparse;
-./uhr_sparse time_query_sparse_table.csv 32 1 33554432 2;
+./uhr_sparse time_query_sparse_table.csv 32 1 1073741824 2;
