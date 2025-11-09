35f92d1aea248c40ab85d53180471ed3c0523301
AlvaroGuzman <aguzman2017@udec.cl>
2025-11-08 22:55:08 -0300

    Boletin 3 y 4

--- 4/sparse_table.cpp
+++ 4/sparse_table.cpp
@@ -20,88 +20,63 @@
 template<class T>
 class sparse_table
 {
-    private:
-        std::vector<std::vector<T> > ST;
-        std::function<T(T, T)> f;
+private:
+  std::vector<std::vector<T> > ST;
 
-        // Returns floor(lg(n))
-        inline uint64_t _lg(uint64_t n)
-        {
-            if (n == 0) {
-                std::cerr << "lg(0) is undefined." << std::endl;
-                std::exit(EXIT_FAILURE);
-            }
+  // Returns floor(lg(n))
+  inline uint64_t _lg(uint64_t n)
+  {
+    if (n == 0) {
+      std::cerr << "lg(0) is undefined." << std::endl;
+      std::exit(EXIT_FAILURE);
+    }
 
-            return std::bit_width(n) - 1;
-        }
+    return std::bit_width(n) - 1;
+  }
 
-    public:
-        sparse_table(const std::vector<T> &A, std::function<T(T, T)> F)
-        {
-            if (A.size() == 0) {
-                std::cerr << "Invalid input array: must be non-empty." << std::endl;
-                std::exit(EXIT_FAILURE);
-            }
+public:
+  //sparse_table(const std::vector<T> &A, std::function<T(T, T)> F)
+  sparse_table(const std::vector<T> &A)
+  {
+    if (A.size() == 0) {
+      std::cerr << "Invalid input array: must be non-empty." << std::endl;
+      std::exit(EXIT_FAILURE);
+    }
 
-            uint64_t i, j, m, n, p, q, one;
-            n = A.size();
-            m = _lg(n);
-            one = 1;
+    uint64_t i, j, m, n, p, q, one;
+    n = A.size();
+    m = _lg(n);
+    one = 1;
 
-            ST.resize(m + 1, std::vector<T>(n));
-            f = F;
+    ST.resize(m + 1, std::vector<T>(n));
 
-            // Store results for length one ranges
-            for (j = 0; j < n; j++)
-                ST[0][j] = A[j];
+    // Store results for length one ranges
+    for (j = 0; j < n; j++)
+      ST[0][j] = A[j];
 
-            // Compute results for queries of length equal to power of two
-            for (i = 0; i < m; i++) {
-                p = one << i;
-                q = p << 1;
-                for (j = 0; j <= n - q; j++)
-                    ST[i + 1][j] = f(ST[i][j], ST[i][j + p]);
-            }
-        }
+    // Compute results for queries of length equal to power of two
+    for (i = 0; i < m; i++) {
+      p = one << i;
+      q = p << 1;
+      for (j = 0; j <= n - q; j++)
+        //ST[i + 1][j] = f(ST[i][j], ST[i][j + p]);
+        ST[i + 1][j] = std::min(ST[i][j], ST[i][j + p]);
+    }
+  }
 
-        // Answers A[i:j] queries in O(lg n) time
-        T query(uint64_t i, uint64_t j)
-        {
-            if (i >= j || j > ST[0].size()) {
-                std::cerr << "Invalid query " << i << " " << j << std::endl;
-                std::exit(EXIT_FAILURE);
-            }
+  T RMQ(uint64_t i, uint64_t j)
+  {
+    if (i >= j || j > ST[0].size()) {
+      std::cerr << "Invalid query " << i << " " << j << std::endl;
+      std::exit(EXIT_FAILURE);
+    }
 
-            uint64_t one, b;
-            one = 1;
+    uint64_t one, b;
+    one = 1;
+    b = _lg(j - i);
 
-            b = _lg(j - i);
-            T q = ST[b][i];
-            i += (one << b);
-
-            while (i < j) {
-                b = _lg(j - i);
-                q = f(q, ST[b][i]);
-                i += (one << b);
-            }
-
-            return q;
-        }
-
-        // Answers A[i:j] queries in constant time if f is idempotent and commutative
-        T query_idempotent(uint64_t i, uint64_t j)
-        {
-            if (i >= j || j > ST[0].size()) {
-                std::cerr << "Invalid query " << i << " " << j << std::endl;
-                std::exit(EXIT_FAILURE);
-            }
-
-            uint64_t one, b;
-            one = 1;
-            b = _lg(j - i);
-
-            return f(ST[b][i], ST[b][j - (one << b)]);
-        }
+    return std::min(ST[b][i], ST[b][j - (one << b)]);
+  }
 };
 
 #endif
